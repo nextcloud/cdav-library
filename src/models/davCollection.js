f2babab16d5aca301cad56bf34128a17323250d7
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-09-25 12:26:34 +0200

    Find backslash fix
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- src/models/davCollection.js
+++ src/models/davCollection.js
@@ -140,7 +140,7 @@ export class DavCollection extends DAVEventListener {
 		const uri = this._getAvailableNameFromToken(name);
 		const data = XMLUtility.serialize(skeleton);
 		await this._request.mkCol(this.url + uri, {}, data);
-		return this.find(uri);
+		return this.find(uri + '/');
 	}
 
 	/**
