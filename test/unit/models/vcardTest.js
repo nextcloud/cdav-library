5dce978dc0004cc9699be428de5f768ed9d47923
Georg Ehrke <developer@georgehrke.com>
2018-10-08 20:58:47 +0200

    Test VCard, VObject
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

--- test/unit/models/vcardTest.js
+++ test/unit/models/vcardTest.js
@@ -21,14 +21,41 @@
  *
  */
 
+import {DavObject} from "../../../src/models/davObject.js";
+import {VCard} from "../../../src/models/vcard.js";
+
 describe('VCard model', () => {
 
 	it('should inherit from DavObject', () => {
-		pending();
+		const parent = jasmine.createSpyObj('DavCollection', ['findAll', 'findAllByFilter', 'find',
+			'createCollection', 'createObject', 'update', 'delete', 'isReadable', 'isWriteable']);
+		const request = jasmine.createSpyObj('Request', ['propFind', 'put', 'delete']);
+		const url = '/foo/bar/file';
+		const props = {
+			'{DAV:}getetag': '"etag foo bar tralala"',
+			'{DAV:}getcontenttype': 'text/blub',
+			'{DAV:}resourcetype': [],
+			'{urn:ietf:params:xml:ns:carddav}address-data': 'FOO BAR BLA BLUB',
+		};
+
+		const vcard = new VCard(parent, request, url, props);
+		expect(vcard).toEqual(jasmine.any(DavObject));
 	});
 
 	it('should expose the address-data as a property', () => {
-		pending();
+		const parent = jasmine.createSpyObj('DavCollection', ['findAll', 'findAllByFilter', 'find',
+			'createCollection', 'createObject', 'update', 'delete', 'isReadable', 'isWriteable']);
+		const request = jasmine.createSpyObj('Request', ['propFind', 'put', 'delete']);
+		const url = '/foo/bar/file';
+		const props = {
+			'{DAV:}getetag': '"etag foo bar tralala"',
+			'{DAV:}getcontenttype': 'text/blub',
+			'{DAV:}resourcetype': [],
+			'{urn:ietf:params:xml:ns:carddav}address-data': 'FOO BAR BLA BLUB',
+		};
+
+		const vcard = new VCard(parent, request, url, props);
+		expect(vcard.data).toEqual('FOO BAR BLA BLUB');
 	});
 
 });
