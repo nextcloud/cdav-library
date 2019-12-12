2def7881d0d16b2966148b9c1ac1f0ab73557e06
Georg Ehrke <developer@georgehrke.com>
2019-12-12 10:11:17 +0100

    Add support for RFC6638 and RFC7953
    Signed-off-by: Georg Ehrke <developer@georgehrke.com>

--- test/unit/models/scheduleInboxTest.js
+++ test/unit/models/scheduleInboxTest.js
@@ -21,8 +21,35 @@
  *
  */
 
+import ScheduleInbox from "../../../src/models/scheduleInbox.js";
+import { Calendar } from "../../../src/models/calendar.js";
+
 describe('Schedule inbox model', () => {
-	it('should ...', () => {
-		pending();
+
+	it('should inherit from Calendar', () => {
+		const parent = jasmine.createSpyObj('DavCollection', ['findAll', 'findAllByFilter', 'find',
+			'createCollection', 'createObject', 'update', 'delete', 'isReadable', 'isWriteable']);
+		const request = jasmine.createSpyObj('Request', ['propFind', 'put', 'delete']);
+		const url = '/foo/bar/folder';
+		const props = {
+			'{urn:ietf:params:xml:ns:caldav}calendar-availability': 'VAVAILABILITY123'
+		}
+
+		const scheduleInbox = new ScheduleInbox(parent, request, url, props);
+		expect(scheduleInbox).toEqual(jasmine.any(Calendar))
 	});
+
+	it('should inherit expose the property calendar-availability', () => {
+		const parent = jasmine.createSpyObj('DavCollection', ['findAll', 'findAllByFilter', 'find',
+			'createCollection', 'createObject', 'update', 'delete', 'isReadable', 'isWriteable']);
+		const request = jasmine.createSpyObj('Request', ['propFind', 'put', 'delete']);
+		const url = '/foo/bar/folder';
+		const props = {
+			'{urn:ietf:params:xml:ns:caldav}calendar-availability': 'VAVAILABILITY123'
+		}
+
+		const scheduleInbox = new ScheduleInbox(parent, request, url, props);
+		expect(scheduleInbox.availability).toEqual('VAVAILABILITY123');
+	});
+
 });
